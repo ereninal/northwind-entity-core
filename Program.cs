c671e1d16bb4327c899f4cfdbff5b10221017e7a
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-25 16:34:00 +0300

    project files created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace database_existing_entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
